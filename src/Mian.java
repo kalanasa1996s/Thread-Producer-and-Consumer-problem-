2d3b8a5564db0f4fd46c8fe87ad3201d7eb95a39
kalanasa1996s <kalanasa1996@gmail.com>
2022-03-31 19:10:58 +0530

    create thread and override methods in runnbalae interface

--- src/Mian.java
+++ src/Mian.java
@@ -1,5 +1,33 @@
 public class Mian {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
 
+        Prodcon prodcon = new Prodcon();
+
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    prodcon.producer();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        Thread thread1 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    prodcon.consumer();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        thread.start();
+        thread1.start();
+        thread.join();
+        thread1.join();
     }
 }
